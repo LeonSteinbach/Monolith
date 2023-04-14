2b80db77a2fc97895dd2771729e65a7642e04b17
LeonSteinbach <leon@steinba.de>
2023-04-14 22:33:10 +0200

    Added particle emitter

---
+++ Monolith/src/particles/MParticle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Monolith.particles;
+
+public class MParticle
+{
+	private readonly Texture2D texture;
+	private Color color;
+	private Vector2 position, velocity;
+	private readonly DateTime createdTime;
+	private readonly float timeToLive;
+	private float speed;
+	private readonly float speedDelta;
+	private float angle;
+	private readonly float angleDelta;
+	private float scale;
+	private readonly float scaleDelta;
+	private float opacity;
+	private readonly float opacityDelta;
+	private readonly float layer;
+
+	public MParticle(Texture2D texture, Vector2 position, Vector2 velocity, float timeToLive)
+	{
+		this.texture = texture;
+		this.position = position;
+		this.velocity = velocity;
+		this.timeToLive = timeToLive;
+
+		createdTime = DateTime.Now;
+		color = Color.White;
+		speed = 1f;
+		speedDelta = 1f;
+		angle = 0f;
+		angleDelta = 1f;
+		scale = 1f;
+		scaleDelta = 1f;
+		opacity = 1f;
+		opacityDelta = 1f;
+		layer = 1f;
+	}
+
+	public MParticle(Texture2D texture, Vector2 position, Vector2 velocity, float timeToLive, 
+		Color color, float speed, float speedDelta, float angle, float angleDelta, 
+		float scale, float scaleDelta, float opacity, float opacityDelta, float layer)
+	{
+		this.texture = texture;
+		this.position = position;
+		this.velocity = velocity;
+		this.timeToLive = timeToLive;
+
+		createdTime = DateTime.Now;
+		this.color = color;
+		this.speed = speed;
+		this.speedDelta = speedDelta;
+		this.angle = angle;
+		this.angleDelta = angleDelta;
+		this.scale = scale;
+		this.scaleDelta = scaleDelta;
+		this.opacity = opacity;
+		this.opacityDelta = opacityDelta;
+		this.layer = layer;
+	}
+
+	private float Age => (float) (DateTime.Now - createdTime).TotalMilliseconds;
+
+	public bool ShouldBeRemoved => Age >= timeToLive || scale <= 0 || opacity <= 0;
+
+	public void Update(GameTime gameTime)
+	{
+		speed += speedDelta * gameTime.ElapsedGameTime.Milliseconds;
+		velocity *= speed * gameTime.ElapsedGameTime.Milliseconds;
+		position += velocity * gameTime.ElapsedGameTime.Milliseconds;
+		angle += angleDelta * gameTime.ElapsedGameTime.Milliseconds;
+		scale += scaleDelta * gameTime.ElapsedGameTime.Milliseconds;
+		opacity += opacityDelta * gameTime.ElapsedGameTime.Milliseconds;
+
+		color.A = (byte) opacity;
+	}
+
+	public void Render(SpriteBatch spriteBatch)
+	{
+		spriteBatch.Draw(texture, position, null, color, angle, texture.Bounds.Center.ToVector2(), scale, SpriteEffects.None, layer);
+	}
+}
---
+++ Monolith/src/particles/MParticleEmitter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Monolith.particles;
+
+public class MParticleEmitter
+{
+	private DateTime timer;
+	private readonly List<MParticle> particles;
+
+	public MParticleEmitter()
+	{
+		timer = DateTime.Now;
+		particles = new List<MParticle>();
+	}
+
+	public void Emit(int delay, Texture2D texture, Vector2 position, Vector2 velocity, float timeToLive)
+	{
+		DateTime now = DateTime.Now;
+		if ((now - timer).TotalMilliseconds >= delay) {
+			timer = now;
+			MParticle particle = new MParticle(texture, position, velocity, timeToLive);
+			particles.Add(particle);
+		}
+	}
+
+	public void Emit(int delay, Texture2D texture, Vector2 position, Vector2 velocity, float timeToLive,
+		Color color, float speed, float speedDelta, float angle, float angleDelta,
+		float scale, float scaleDelta, float opacity, float opacityDelta, float layer)
+	{
+		DateTime now = DateTime.Now;
+		if ((now - timer).TotalMilliseconds >= delay) {
+			timer = now;
+			MParticle particle = new MParticle(texture, position, velocity, timeToLive, color, speed, 
+				speedDelta, angle, angleDelta, scale, scaleDelta, opacity, opacityDelta, layer);
+			particles.Add(particle);
+		}
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		for (int i = particles.Count - 1; i >= 0; i--) {
+			particles[i].Update(gameTime);
+			if (particles[i].ShouldBeRemoved)
+				particles.Remove(particles[i]);
+		}
+	}
+
+	public void Render(SpriteBatch spriteBatch)
+	{
+		for (int i = particles.Count - 1; i >= 0; i--) {
+			particles[i].Render(spriteBatch);
+		}
+	}
+}
