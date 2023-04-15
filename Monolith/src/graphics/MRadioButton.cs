54ee783d5c64c2874631ceb90f8d87ff7c9991fd
LeonSteinbach <leon@steinba.de>
2023-04-15 18:51:35 +0200

    Added radio group and button

---
+++ Monolith/src/graphics/MRadioButton.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monolith.assets;
+using Monolith.input;
+
+namespace Monolith.graphics;
+
+public class MRadioButton
+{
+    private readonly MStaticSprite defaultSprite, hoverSprite, checkedSprite, hoverCheckedSprite;
+    private readonly string text;
+    private readonly SpriteFont font;
+    private readonly Color color;
+    private readonly string hoverSound, clickSound;
+
+    private bool isHovering, wasHovering;
+    private bool isPressed, wasPressed;
+    private bool isChecked;
+
+    public event Action<MRadioButton> OnSelected;
+
+    public MRadioButton(MStaticSprite defaultSprite, MStaticSprite hoverSprite, MStaticSprite checkedSprite,
+        MStaticSprite hoverCheckedSprite, string text = null, SpriteFont font = null, Color color = default,
+        bool isChecked = false, string hoverSound = null, string clickSound = null)
+    {
+        this.defaultSprite = defaultSprite ?? throw new ArgumentNullException(nameof(defaultSprite));
+        this.hoverSprite = hoverSprite ?? throw new ArgumentNullException(nameof(hoverSprite));
+        this.checkedSprite = checkedSprite ?? throw new ArgumentNullException(nameof(checkedSprite));
+        this.hoverCheckedSprite = hoverCheckedSprite ?? throw new ArgumentNullException(nameof(hoverCheckedSprite));
+        this.text = text;
+        this.font = font;
+        this.color = color;
+        this.isChecked = isChecked;
+        this.hoverSound = hoverSound;
+        this.clickSound = clickSound;
+    }
+
+    public Rectangle Rectangle => Hover()
+        ? (isChecked ? hoverCheckedSprite.Hitbox : hoverSprite.Hitbox)
+        : (isChecked ? checkedSprite.Hitbox : defaultSprite.Hitbox);
+
+    public string Text => text;
+
+    public bool Hover() => isHovering;
+
+    public bool Entered() => isHovering && !wasHovering;
+
+    public bool Left() => !isHovering && wasHovering;
+
+    public bool Pressed() => isPressed;
+
+    public bool IsSelected() => isChecked;
+
+    public void Select()
+    {
+        if (!isChecked)
+        {
+            isChecked = true;
+            OnSelected?.Invoke(this);
+        }
+    }
+
+    public void Deselect()
+    {
+        isChecked = false;
+    }
+
+    public void Update()
+    {
+        wasHovering = isHovering;
+        isHovering = Rectangle.Contains(MInput.MousePosition());
+
+        wasPressed = isPressed;
+        isPressed = isHovering && MInput.IsLeftPressed();
+
+        if (Entered() && hoverSound != null)
+        {
+            MAudioManager.PlaySound(hoverSound);
+        }
+
+        if (Pressed())
+        {
+            if (clickSound != null)
+                MAudioManager.PlaySound(clickSound);
+            Select();
+        }
+    }
+
+    public void Render(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        MStaticSprite sprite = Hover() ? 
+            (isChecked ? hoverCheckedSprite : hoverSprite) : 
+            (isChecked ? checkedSprite : defaultSprite);
+
+        sprite.Render(gameTime, spriteBatch);
+    }
+}
---
+++ Monolith/src/graphics/MRadioGroup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Monolith.graphics;
+
+public class MRadioGroup
+{
+	private readonly List<MRadioButton> radioButtons = new ();
+	private MRadioButton selectedButton;
+
+	public void AddButton(MRadioButton button)
+	{
+		radioButtons.Add(button);
+
+		if (button.IsSelected())
+		{
+			if (selectedButton != null)
+				throw new Exception("Only one radio button can be selected!");
+			selectedButton = button;
+		}
+		
+		button.OnSelected += RadioButtonSelected;
+	}
+
+	private void RadioButtonSelected(MRadioButton button)
+	{
+		foreach (MRadioButton otherButton in radioButtons)
+		{
+			if (otherButton != button)
+			{
+				otherButton.Deselect();
+			}
+		}
+
+		selectedButton = button;
+	}
+
+	public string GetSelectedText()
+	{
+		return selectedButton?.Text;
+	}
+
+	public void Update()
+	{
+		foreach (MRadioButton button in radioButtons)
+		{
+			button.Update();
+		}
+	}
+
+	public void Render(GameTime gameTime, SpriteBatch spriteBatch)
+	{
+		foreach (MRadioButton button in radioButtons)
+		{
+			button.Render(gameTime, spriteBatch);
+		}
+	}
+}
